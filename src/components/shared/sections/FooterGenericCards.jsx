e894779979f56f9bc5d0ed2e3d113ad29df9bb31
Salekin Imran <abir045@gmail.com>
2025-12-03 14:01:23 +0600

    updated getAchievementData import path in FooterGenericCards

--- src/components/shared/sections/FooterGenericCards.jsx
+++ src/components/shared/sections/FooterGenericCards.jsx
@@ -1,48 +1,5 @@
-// // import Heading from "@/components/typography/Heading";
-// import { getAchievementData } from "../../../graphql/components/getAchievementData";
-// import Heading from "../../typography/Heading";
-// import PlainGridRenderer from "../PlainGridRenderer";
-// import AchievementCard from "../cards/AchievementCard";
-// // import { getAchievementData } from "@/graphql/Components/getAchievementData";
-
-// const CARD_COMPONENTS = {
-//   Achievement: AchievementCard,
-// };
-
-// const FooterGenericCards = async ({ data }) => {
-//   const achievementIds = data.data.card_data.map(
-//     (achievement) => achievement.id
-//   );
-//   const achievementsData = await getAchievementData(achievementIds);
-
-//   const CardComponent = CARD_COMPONENTS[data?.data?.card_item] || (() => null);
-
-//   return (
-//     <section className="mx-auto max-w-[1680px]">
-//       {data.data.section_title && (
-//         <Heading
-//           tag="h2"
-//           variant="h2"
-//           className="text-app-dark mb-[30px] lg:mb-[60px]"
-//         >
-//           {data?.data?.section_title}
-//         </Heading>
-//       )}
-//       <PlainGridRenderer
-//         itemsData={achievementsData}
-//         renderItem={(item) => <CardComponent data={item.node} />}
-//         itemsPerRow={data?.data?.items_per_row}
-//         footerCTA={data?.data?.footer_cta[0]}
-//         className={data?.data?.classnames}
-//       />
-//     </section>
-//   );
-// };
-
-// export default FooterGenericCards;
-
 import { useEffect, useState } from "react";
-import { getAchievementData } from "../../../graphql/components/getAchievementData";
+import { getAchievementData } from "../../../graphql/Components/getAchievementData";
 import Heading from "../../typography/Heading";
 import PlainGridRenderer from "../PlainGridRenderer";
 import AchievementCard from "../cards/AchievementCard";
